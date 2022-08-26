1801e7121f3de867df0d89ee603c78e855bfc596
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2022-08-26 16:27:39 +0300

    first

--- src/Task1.java
+++ src/Task1.java
@@ -1,33 +1,38 @@
 public class Task1 {
     public static int numberOfSymbol(String str, char symbol) {
-        int lenSymbol = str.length();
-        int lenWithoutSymbol = str.replace(symbol+"", "").length();
-        return lenSymbol-lenWithoutSymbol;
+        return (int) str.chars().filter(ch -> ch == symbol).count();
     }
-    public static boolean isGreat(String str1, String str2, char symbol) {
+
+    public static boolean isGreatBySymbol(String str1, String str2, char symbol) {
         return numberOfSymbol(str1, symbol) > numberOfSymbol(str2, symbol);
     }
 
     public static void main(String[] args) {
-        String[] arr = {"сafe", "сheck", "ссс", "сссс" , "сollect", "сall"};
-        char с = 'с';
+        String[] arr = {"aaa", "check", "ccc", "cccc", "collect", "call"};
+        char symbol = 'c';
         System.out.println("Before: ");
-        for (String str : arr) {
-            System.out.println(str + " " + numberOfSymbol(str, с));
-        }
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (isGreat(arr[j], arr[j+1], с)) {
-                    var tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
-                }
-            }
-        }
+        outputResult(arr,symbol);
+        sortBySymbol(arr, symbol);
         System.out.println();
         System.out.println("After: ");
-        for (String str : arr) {
-            System.out.println(str + " " + numberOfSymbol(str, с));
+        outputResult(arr, symbol);
+    }
+
+    public static void outputResult(String[] strings, char symbol) {
+        for (String str : strings) {
+            System.out.println(str + " " + numberOfSymbol(str, symbol));
+        }
+    }
+
+    public static void sortBySymbol(String[] strings, char symbol) {
+        for (int i = 0; i < strings.length - 1; i++) {
+            for (int j = 0; j < strings.length - i - 1; j++) {
+                if (isGreatBySymbol(strings[j], strings[j + 1], symbol)) {
+                    String tmp = strings[j];
+                    strings[j] = strings[j + 1];
+                    strings[j + 1] = tmp;
+                }
+            }
         }
     }
 }
