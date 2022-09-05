ad89339fe5c9d067496aa3b65158a1bbc5d3ad78
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2022-09-05 20:14:08 +0300

    first

--- src/Task5.java
+++ src/Task5.java
@@ -4,22 +4,22 @@ public class Task5 {
         System.out.println("Before");
         final int[][] a = new int[n][n];
         for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
+            for (int j = 0; j < a[i].length; j++) {
                 a[i][j] = n * i + j;
                 System.out.printf("%4d", a[i][j]);
             }
             System.out.println();
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
                 final int temp = a[i][j];
                 a[i][j] = a[j][i];
                 a[j][i] = temp;
             }
         }
         System.out.println("After");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
                 System.out.printf("%3d", a[i][j]);
             }
             System.out.println();
